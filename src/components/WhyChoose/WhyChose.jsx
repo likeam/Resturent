a94167d0054603aa32d64ce1fe9788b6b9395208
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-15 10:24:59 +0500

    Adding Annimation to WhyChoose Section

--- src/components/WhyChoose/WhyChose.jsx
+++ src/components/WhyChoose/WhyChose.jsx
@@ -6,11 +6,16 @@ const WhyChoose = () => {
     <>
       <div className=" py-14 md:py-28 bg-gray-50">
         <div className="container ">
-          <h1 className="py-8 text-2xl font-semibold tracking-wider text-center text-dark">
+          <h1 
+            data-aos="fade"            
+            className="py-8 text-2xl font-semibold tracking-wider text-center text-dark">
             {" "}
             Why Choose Us
           </h1>
-          <div className="flex flex-row p-10">
+          <div 
+            data-aos="fade"
+            data-aos-delay="300"
+            className="flex flex-row p-10">
             <div className="grid grid-cols-2 md:grid-cols-4 gap-14 sm:gap-4">
               <div className="flex flex-col items-center justify-center gap-2 px-2 text-center text-dark">
                 <p className="font-bold text-dark/70 min-w-40">
